635c37193ee006760da510bdeac2376eef99fb89
adam-talbot <adam.talbot527@gmail.com>
2022-12-01 21:53:14 -0700

    completed two more sql questions from danny's diner case study

--- case_study_1_dannys_diner.sql
+++ case_study_1_dannys_diner.sql
@@ -1,10 +1,10 @@
 /* --------------------
-  Case Study #1 - Danny's Din
+  Case Study #1 - Danny's Diner
    --------------------*/
 
--- 1. What is the total amount each customer spent at the restaurant?
--- 2. How many days has each customer visited the restaurant?
--- 3. What was the first item from the menu purchased by each customer?
+-- 1. What is the total amount each customer spent at the restaurant? *
+-- 2. How many days has each customer visited the restaurant? *
+-- 3. What was the first item from the menu purchased by each customer? *
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 -- 5. Which item was the most popular for each customer?
 -- 6. Which item was purchased first by the customer after they became a member?
@@ -29,3 +29,39 @@ from dannys_diner.sales
 join dannys_diner.menu
 	using(product_id)
 group by customer_id;
+
+-- 2. How many days has each customer visited the restaurant?
+select customer_id, count(distinct order_date) -- need to count distinct to account for multiple visits on same day
+from dannys_diner.sales
+group by customer_id;
+
+-- 3. What was the first item from the menu purchased by each customer?
+-- partition by customer_id, order by order_date, take the first row of each partition
+
+-- using subquery
+select customer_id, product_name
+from (
+  select
+    *,
+    DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY order_date) AS order_num -- use dense rank since just have day and could be multiple orders on same day
+  from dannys_diner.sales
+  join dannys_diner.menu using(product_id)
+  ) as t1 -- have to use a subquery due to order of operations since where is evaluated before select
+where order_num = 1
+group by customer_id, product_name -- group by to get unique combinations of customer and order in case same item was ordered twice on same day (don't want to see both in result)
+;
+
+-- using CTE (common table expression)
+with ordered_sales as
+(
+  select
+    *,
+    DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY order_date) AS order_num
+  from dannys_diner.sales
+  join dannys_diner.menu using(product_id)
+)
+select customer_id, product_name
+from ordered_sales
+where order_num = 1
+group by customer_id, product_name
+; -- looks a little cleaner and more readable
