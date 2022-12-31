adc599742eb7c9c1ab2106895c6cb7d3e7999471
adam-talbot <adam.talbot527@gmail.com>
2022-12-30 22:39:37 -0700

    finished case study question using week() function

--- case_study_2_pizza_runner.sql
+++ case_study_2_pizza_runner.sql
@@ -426,3 +426,29 @@ select
 from customer_orders_clean
 group by 1
 order by 1;
+
+-- B. Runner and Customer Experience
+-- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+-- 2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+-- 3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
+-- 4. What was the average distance travelled for each customer?
+-- 5. What was the difference between the longest and shortest delivery times for all orders?
+-- 6. What was the average speed for each runner for each delivery and do you notice any trend for these values?
+-- 7. What is the successful delivery percentage for each runner?
+
+-- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+-- Pull week number from timestamp and group by that new column
+-- check dtypes of columns in runners table
+SELECT
+	*
+FROM
+    information_schema.columns
+WHERE
+    table_name = 'runners'; -- date type present for registration_date column
+
+select
+	week(registration_date) as week,
+  count(runner_id) as new_runner_count
+from runners
+group by 1
+order by 1;
